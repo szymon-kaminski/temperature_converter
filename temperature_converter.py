feb06417f05c9e6a5822e21029ef81b52b628d64
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-07-08 15:11:50 +0200

    Add main() function

--- temperature_converter.py
+++ temperature_converter.py
@@ -4,3 +4,13 @@ def celsius_to_fahrenheit(celsius):
 
 def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5 / 9
+
+
+def main():
+    print("Welcome to the Temperature Converter!")
+    print("1. Celsius to Fahrenheit")
+    print("2. Fahrenheit to Celsius")
+
+
+if __name__ == "__main__":
+    main()
